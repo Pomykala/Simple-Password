027cb0b1c1caa1d74b46c8061535d79b6422a231
pomykala <8mpomykala@stpatrick.org>
2017-04-26 11:24:42 -0500

    Add files via upload

---
+++ simplepass.js
@@ -0,0 +1,19 @@
+//type checkString below
+function checkString(str)
+
+
+
+
+
+
+
+//type getString below
+function getString(n) {try {
+
+} catch (e) {
+  console.log(e.message);
+} finally {
+
+}
+
+}
